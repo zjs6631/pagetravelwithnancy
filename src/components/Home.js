a7732e869a8e41c1f9d68d35638475f2b5b41b64
zjs6631 <zacharyjonessmith@gmail.com>
2022-12-20 16:10:08 -0500

    added filler content to show examples

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,31 +1,51 @@
 import React from 'react';
+import examplePic from '../images/exampleBlog.jpg';
+import exampleBook from '../images/exampleBook.jpg';
 
 const Home = () =>{ 
     return(
         <div id='Home'>
             <div id='HomeCurrentRead'>
-                <p className='sectionHeader'>Currently reading:</p> 
+                <p className='sectionHeader'>Currently reading:</p>
+                <div className='currentReadContainer'> 
                 <div className='dummyRead'>
-                    <p>Book title:</p>
-                    <p>Book img</p>
+                    <p>Accidentally Amy</p>
+                    <img src={exampleBook}/>
                 </div>
                 <div className='dummyRead'>
-                    <p>Book title:</p>
-                    <p>Book img</p>
+                    <p>Accidentally Amy</p>
+                    <img src={exampleBook}/>
                 </div>
-                <p>----------------</p>
+                </div>
             </div>
             <div id='HomeRecentPosts'>
                 <p className='sectionHeader'>Recent blog posts:</p>
+                <div className='blogContainer'>
                 <div className='dummyBlog'>
-                    <p className='blogTitle'>Blog #1</p>
+                    <img src={examplePic}></img>
                     <p className='blogDescrip'>This was a good book.</p>
                     <button className='dummyBlogButton'>Read More! -></button>
                 </div>
                 <div className='dummyBlog'>
-                    <p className='blogTitle'>Blog #2</p>
+                    <img src={examplePic}></img>
                     <p className='blogDescrip'>This was a bad book.</p>
                     <button className='dummyBlogButton'>Read More! -></button>
+                </div>
+                <div className='dummyBlog'>
+                    <img src={examplePic}></img>
+                    <p className='blogDescrip'>This was a bad book.</p>
+                    <button className='dummyBlogButton'>Read More! -></button>
+                </div>
+                <div className='dummyBlog'>
+                    <img src={examplePic}></img>
+                    <p className='blogDescrip'>This was a bad book.</p>
+                    <button className='dummyBlogButton'>Read More! -></button>
+                </div>
+                <div className='dummyBlog'>
+                    <img src={examplePic}></img>
+                    <p className='blogDescrip'>This was a bad book.</p>
+                    <button className='dummyBlogButton'>Read More! -></button>
+                </div>
                 </div>
             </div>
             <div id='HomeIGPosts'>IG posts:</div>
